62ddd939947abe5f0e34006aadbbdbb3985827ed
Matthew Mosesohn <matthew.mosesohn@gmail.com>
2019-02-24 17:57:27 +0300

    add peer and cert dist

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update && \
 sudo apt-get install -y vim python-pip python-virtualenv git && \
-echo virtualenv /root/venv && \
-. /root/venv/bin/activate && \
-git clone https://github.com/mattymo/etcd-experiment /root/etcd
-git clone https://github.com/kubernetes-sigs/kubespray /root/etcd/kubespray && \
-pip install -r /root/kubespray/requirements.txt
+virtualenv ~/venv && \
+. ~/venv/bin/activate && \
+git clone https://github.com/mattymo/etcd-experiment etcd || : && \
+git clone https://github.com/kubernetes-sigs/kubespray etcd/kubespray || : && \
+pip install -r etcd/kubespray/requirements.txt
